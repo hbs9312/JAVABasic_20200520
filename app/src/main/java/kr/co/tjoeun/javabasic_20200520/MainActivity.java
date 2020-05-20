8642362bd3a927c37815e18e5185574168002493
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-20 16:38:58 +0900

    데이터바인딩 세팅

--- app/src/main/java/kr/co/tjoeun/javabasic_20200520/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/javabasic_20200520/MainActivity.java
@@ -1,29 +1,23 @@
 package kr.co.tjoeun.javabasic_20200520;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import kr.co.tjoeun.javabasic_20200520.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText contentEdt;
-    Button okBtn;
-    TextView contentTxt;
-
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        contentEdt = findViewById(R.id.contentEdt);
-        okBtn = findViewById(R.id.okBtn);
-        contentTxt = findViewById(R.id.contentTxt);
-
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     }
 }
